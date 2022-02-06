a224f661329b7821d79cc8211899b71534398ba3
Camilla Gestin <camilla.gestrin@chasacademy.se>
2022-02-06 18:00:36 +0100

    Changes 2.0

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+  function setAction(form) {
+    form.action = "register.html";
+    alert("Welcome to the world of Second Space! Exciting things ahead! We'll keep you updated.");
+    return false;
+  }
